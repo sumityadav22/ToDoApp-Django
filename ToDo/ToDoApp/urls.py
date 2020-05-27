038a7c2626bad161b182b203d3a9de5fc1fdd7f4
sumityadav22 <sumityadavs5574@gmail.com>
2020-05-27 19:04:01 +0530

    ADD - deleteToDo

--- ToDo/ToDoApp/urls.py
+++ ToDo/ToDoApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AddToDo, Home
+from .views import AddToDo, DeleteToDo, Home
 
 urlpatterns = [
     path('',Home,name = 'home'),
     path('add_todo/',AddToDo,name = 'add_todo'),
+    path('delete_todo/<int:todo_id>/',DeleteToDo,name = 'delete_todo'),
 ]
--- ToDo/ToDoApp/views.py
+++ ToDo/ToDoApp/views.py
@@ -15,3 +15,7 @@ def AddToDo(request):
     if form_ob.is_valid():
         form_ob.save()
     return redirect('home')
+
+def DeleteToDo(request, todo_id):
+    ToDoModel.objects.get(id = todo_id).delete()
+    return redirect('home')
