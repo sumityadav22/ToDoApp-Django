19f95d7330d738858aae09ed2cde70ec9f63427e
sumityadav22 <sumityadavs5574@gmail.com>
2020-05-28 22:55:34 +0530

    ADD - text to speech

--- ToDo/ToDoApp/urls.py
+++ ToDo/ToDoApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import addToDo, deleteToDo, home
+from .views import addToDo, deleteToDo, home, speak
 
 urlpatterns = [
     path('',home,name = 'home'),
     path('add_todo/',addToDo,name = 'add_todo'),
     path('delete_todo/<int:todo_id>/',deleteToDo,name = 'delete_todo'),
+    path('speak/<int:todo_id>/',speak,name = 'speak'),
 ]
--- ToDo/ToDoApp/views.py
+++ ToDo/ToDoApp/views.py
@@ -1,3 +1,5 @@
+import os
+from gtts import gTTS
 from django.shortcuts import redirect, render
 from .forms import ToDoForm
 from .models import ToDoModel
@@ -19,4 +21,11 @@ def addToDo(request):
 
 def deleteToDo(request, todo_id):
     ToDoModel.objects.get(id = todo_id).delete()
+    return redirect('home')
+
+def speak(request, todo_id):
+    todo = ToDoModel.objects.get(id = todo_id)
+    text_to_speech = gTTS(text=todo.text, lang='en-uk')
+    text_to_speech.save("voice.mp3")
+    os.system("start voice.mp3")
     return redirect('home')
